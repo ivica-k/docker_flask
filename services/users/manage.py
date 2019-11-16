8f0d6da459ff65e97ee8736ede77723daa73ca1a
ivica-k <ivica.kolenkas@gmail.com>
2019-11-16 17:22:52 +0100

    converts flask app to use blueprints

--- services/users/manage.py
+++ services/users/manage.py
@@ -2,16 +2,21 @@ import sys
 import unittest
 
 from flask.cli import FlaskGroup
-from project import app, db
 
-cli = FlaskGroup(app)
+from project import create_app, db
+from project.api.models import User
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
+
 
 @cli.command("recreate_db")
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
-    
+
+
 @cli.command()
 def test():
     """Runs the tests without code coverage"""
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,35 +1,34 @@
 import os
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
+
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-# instantiate the app
-app = Flask(__name__)
-app_settings = os.getenv("APP_SETTINGS")
-app.config.from_object(app_settings)
-
-api = Api(app)
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
-
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+from project.api.users import users_blueprint
 
 
-class UsersPing(Resource):
-    def get(self):
-        return {
-        "status": "success",
-        "message": "pong!"
-    }
+# instantiate the db
+db = SQLAlchemy()
 
 
-api.add_resource(UsersPing, "/users/ping")
+# new
+def create_app(script_info=None):
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv("APP_SETTINGS")
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {"app": app, "db": db}
+
+    return app
